6c0dcf1d4f7e42e5fcea9bc0bedd3aa4436d527c
Pin4in <sergii.korkh@gmail.com>
2016-03-01 20:13:30 +0200

    add collection Transactions

---
+++ collections/transactions.js
@@ -0,0 +1,31 @@
+Transactions = new Mongo.Collection('transactions');
+
+TransactionSchema = new SimpleSchema({
+
+    name: {
+        type: String,
+        label: "Name"
+    },
+    desc: {
+        type: String,
+        label: "Description"
+    },
+    author: {
+        type: String,
+        label: "Author",
+        autoValue: function() {
+            return this.userId
+        }
+    },
+    createdAt: {
+        type: Date,
+        label: "Created At",
+        autoValue: function() {
+            return new Date()
+        }
+    }
+
+});
+
+
+Transactions.attachSchema(TransactionSchema);
